abcb36fc1ba3d127d62c87f1aa3f58ae04715135
binjo <binjo.cn@gmail.com>
2010-01-06 14:20:55 +0800

    decompress SWF in python

---
+++ cws2fws.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding : utf-8 -*-
+# cws2fws.py
+# Binjo @ 2009-09-05 11:13:08
+#-------------------------------------------------------------------------------
+from __future__ import with_statement
+from zlib       import decompress
+import sys
+
+def main():
+    """TODO
+    """
+    with open(sys.argv[1], 'rb') as fh:
+        c = fh.read()
+
+    if c[:3] != 'CWS':
+        exit( '[-] File is not compressed...' )
+
+    ver = c[3]
+
+    nc  = decompress( c[8:] )
+
+    with open(sys.argv[2], 'wb') as fh:
+        fh.write( 'FWS' + ver + c[4:8] + nc )
+        print "[+] Done, check out %s..." % sys.argv[2]
+#-------------------------------------------------------------------------------
+if __name__ == '__main__':
+    main()
+#-------------------------------------------------------------------------------
+# EOF
